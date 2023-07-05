8353e85d4e4df3e0c52f38cf57132becb2e2caee
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-05 07:39:39 -0300

    finish basic api

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ import http from 'http'
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain')
-    res.end('Bem-vindo à minha api')
-})
+    res.end('Bem-vindo a minha api')
+});
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Servidor rodando na porta http://localhost:${port}`);
+});
